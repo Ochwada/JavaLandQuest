2c70ed723d405d47c42aa962bd77d363d3d0eff3
ochwada <nakhulolinda@yahoo.com>
2025-05-30 13:08:54 +0200

    DayScheduler  methods added - and documentation

--- src/main/java/com/codeland/DayScheduler.java
+++ src/main/java/com/codeland/DayScheduler.java
@@ -1,5 +1,33 @@
 package com.codeland;
 
+
+
+
+/**
+ * Story: Assign magical activities based on day of the week.
+ *
+ * Task:
+ *
+ * Use switch-case to return a string activity for a given day (1–7).
+ * */
 public class DayScheduler {
+
+    /**
+     * Returns a magical activity based on the day of the week.
+     * @param day The day of the week ( day 1 to 7)
+     * @return A dummy magical activity.
+     * */
+    public static String dayActivities(int day){
+        switch (day){
+            case 1 -> {return "Brew basic healing potions";}
+            case 2 -> {return "Practice Fireball casting";}
+            case 3 -> {return "Summon your sprit animal";}
+            case 4 -> {return "Flying drill";}
+            case 5 -> {return "Quest day";}
+            case 6 -> {return "Weapon enchantment";}
+            case 7 -> {return "Train with enchanted weapons";}
+            default -> {return "Enter a valid day number (1-7)";}
+        }
+    }
     // TODO: Implement switch-case to return activity by day
 }
--- src/main/java/com/codeland/Main.java
+++ src/main/java/com/codeland/Main.java
@@ -1,25 +1,27 @@
 package com.codeland;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to JavaLand: The Code Chronicles!");
 
-        /*int totalUnit = 95;
-        System.out.println("\n1.--- Potion Calculator ---");
-        System.out.println("Full Potions Made: " + PotionCalculator.fullPotionsMade(totalUnit));
-        System.out.println("Leftover Units: " + PotionCalculator.leftOverUnits(totalUnit));
-        System.out.println("Total Ingredients Cost: " + PotionCalculator.totalCostOfIngredients(totalUnit));*/
+        System.out.println("\n5.--- Day Scheduler ---");
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Enter Day Number: ");
+        //int day = scanner.nextInt();
+        int day = 4;
+        System.out.println("Day "+ day + ": " + DayScheduler.dayActivities(day));
 
 
-       /* System.out.println("\n2.--- Spell Cost Estimator ---");
-        System.out.println(SpellCostEstimator.getSpellSummary("Genjustu", 6));
-        System.out.println(SpellCostEstimator.getSpellSummary("Ninjutsu", 4));
-        System.out.println(SpellCostEstimator.getSpellSummary("Taijutsu", 2));
-        System.out.println(SpellCostEstimator.getSpellSummary("Wolf-Feng-Over-Feng", 2));
-        System.out.println();*/
+        System.out.println("\n4.--- Guild Gatekeeper ---");
+        System.out.println("Knight -> " + GuildGatekeeper.determineAccess("Knight"));
+        System.out.println("mage -> " + GuildGatekeeper.determineAccess("mage"));
+        System.out.println("Me -> " + GuildGatekeeper.determineAccess("me"));
+        System.out.println();
 
 
-        System.out.println("\n3.--- Hero Eligibility Checker ---");
+        /*System.out.println("\n3.--- Hero Eligibility Checker ---");
         int age = 20;
         int strength = 50;
         int experience = 55;
@@ -32,15 +34,22 @@ public class Main {
         System.out.println("Eligibility: " + (eligible ? "✅ Eligible for the Guild" : "❌ Not eligible"));
         System.out.println("Rank: " + rank);
         System.out.println("Rank & Eligibility: " + rankAndEligibility);
-        System.out.println();
-
-        System.out.println("\n4.--- Guild Gatekeeper ---");
-        System.out.println("Knight -> " + GuildGatekeeper.determineAccess("Knight"));
-        System.out.println("mage -> " + GuildGatekeeper.determineAccess("mage"));
-        System.out.println("Me -> " + GuildGatekeeper.determineAccess("me"));
+        System.out.println();*/
 
 
+        /* System.out.println("\n2.--- Spell Cost Estimator ---");
+        System.out.println(SpellCostEstimator.getSpellSummary("Genjustu", 6));
+        System.out.println(SpellCostEstimator.getSpellSummary("Ninjutsu", 4));
+        System.out.println(SpellCostEstimator.getSpellSummary("Taijutsu", 2));
+        System.out.println(SpellCostEstimator.getSpellSummary("Wolf-Feng-Over-Feng", 2));
+        System.out.println();*/
 
+
+        /*int totalUnit = 95;
+        System.out.println("\n1.--- Potion Calculator ---");
+        System.out.println("Full Potions Made: " + PotionCalculator.fullPotionsMade(totalUnit));
+        System.out.println("Leftover Units: " + PotionCalculator.leftOverUnits(totalUnit));
+        System.out.println("Total Ingredients Cost: " + PotionCalculator.totalCostOfIngredients(totalUnit));*/
 
 
         // TODO: Start Hero Dashboard or individual module tests
